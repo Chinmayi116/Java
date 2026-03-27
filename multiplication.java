9e8f71565cf73a6a5a82515a458e7ed815453aa6
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-03-27 07:45:55 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,7 @@
+class Multiplication {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        int result = a * b;
+        System.out.println("Multiplication = " + result);
+    }
+}
