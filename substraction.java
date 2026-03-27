dc098c52aa5a97a6e8d3bfd64a74cfa25638efc9
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-03-27 07:45:16 +0530

    Create substraction.java

---
+++ substraction.java
@@ -0,0 +1,7 @@
+class Subtraction {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        int result = a - b;
+        System.out.println("Subtraction = " + result);
+    }
+}
