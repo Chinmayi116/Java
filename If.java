561276385378f49f713e350813a0f7581c57973c
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-03-29 08:34:32 +0530

    Add IfExample class to check if number is positive

---
+++ If.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Number is positive");
+        }
+    }
+}
