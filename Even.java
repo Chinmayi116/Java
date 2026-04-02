33aab316d300e3ca0e7fcf309ff4ec34f318c79a
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-04-02 09:13:45 +0530

    Add EvenNumbers class to print even numbers

---
+++ Even.java
@@ -0,0 +1,12 @@
+class EvenNumbers {
+    public static void main(String[] args) {
+
+        System.out.println("Even numbers from 1 to 20:");
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
