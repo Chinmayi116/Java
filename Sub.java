dbd1fd4379f29e11e4ac5410912e37250e35c26d
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-04-03 08:22:43 +0530

    Create Sub.java

---
+++ Sub.java
@@ -0,0 +1,11 @@
+class Subtraction {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result;
+
+        result = a - b;
+
+        System.out.println("Subtraction = " + result);
+    }
+}
