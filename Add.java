02b0704dd0d6713fe8ebf4d008e505f852706529
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-04-03 08:21:50 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,11 @@
+class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum;
+
+        sum = a + b;
+
+        System.out.println("Addition = " + sum);
+    }
+}
