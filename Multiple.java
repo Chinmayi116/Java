8ac3a6954f28cd27e6c3d218c21b1bd1ab0d1d2d
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-04-03 08:22:14 +0530

    Create Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,11 @@
+class Multiplication {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result;
+
+        result = a * b;
+
+        System.out.println("Multiplication = " + result);
+    }
+}
