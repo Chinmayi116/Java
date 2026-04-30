cfdfb042cb62ac0293dee184c378217a51d60075
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-04-30 22:21:43 +0530

    Add number guessing game in Random.java

---
+++ Random.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int number = rand.nextInt(100) + 1;
+        int guess;
+
+        do {
+            System.out.print("Guess a number (1-100): ");
+            guess = sc.nextInt();
+
+            if (guess > number) {
+                System.out.println("Too high!");
+            } else if (guess < number) {
+                System.out.println("Too low!");
+            } else {
+                System.out.println("Correct! 🎉");
+            }
+        } while (guess != number);
+    }
+}
