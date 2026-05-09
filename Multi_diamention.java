aa0f76318d23d63da7515a24df9fc9264f535c63
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-05-09 17:20:33 +0530

    Create Multi_diamention.java

---
+++ Multi_diamention.java
@@ -0,0 +1,12 @@
+int[][] arr=new int[3][3];//3 row and 3 column 
+Example to initialize Multidimensional Array in Java
+arr[0][0]=1; 
+arr[0][1]=2; 
+arr[0][2]=3; 
+arr[1][0]=4; 
+arr[1][1]=5; 
+arr[1][2]=6; 
+arr[2][0]=7; 
+arr[2][1]=8; 
+arr[2][2]=9; 
+system.out.println(arr[0][1]);
