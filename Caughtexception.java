d7b5f0f5ac0af95101a9b770bd9b3a7bd55f5db4
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-05-17 09:25:07 +0530

    Add exception handling for division by zero

---
+++ Caughtexception.java
@@ -0,0 +1,15 @@
+class Demo {
+    public static void main(String args[]) {
+
+        try {
+            int a = 10 / 0;
+            System.out.println(a);
+        }
+
+        catch(ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+
+        System.out.println("Program continues");
+    }
+}
