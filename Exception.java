e5d0a228655db9cd2e08b79f0b286ade3346f774
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-05-19 22:01:27 +0530

    Create Exception.java

---
+++ Exception.java
@@ -0,0 +1,24 @@
+class ExceptionDemo
+{
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 0;
+
+        try
+        {
+            int c = a / b;
+            System.out.println("Result = " + c);
+        }
+
+        catch(ArithmeticException e)
+        {
+            System.out.println("Cannot divide by zero");
+        }
+
+        finally
+        {
+            System.out.println("Program Ended");
+        }
+    }
+}
