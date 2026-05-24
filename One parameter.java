1e3802350e669c642a16fe0a3b0ff4bbd4ce1a9a
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-05-24 16:00:42 +0530

    Create One parameter.java

---
+++ One parameter.java
@@ -0,0 +1,15 @@
+class Student {
+String name;
+int age;
+Student(String n) {
+name = n;
+age = 0;
+}
+public class Main {
+public static void main(String[] args) {
+Student s2 = new Student("Alice");
+s2.display();
+}
+
+}
+
