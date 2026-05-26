233b4cd63facebc7981e2c913c96e6bc6877cee0
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-05-26 18:44:17 +0530

    Add Animal and Dog classes with constructor messages

---
+++ Super.java
@@ -0,0 +1,12 @@
+class Animal{  
+Animal(){System.out.println(&quot;animal is created&quot;);}  
+}  
+class Dog extends Animal{  
+Dog(){  
+System.out.println(&quot;dog is created&quot;);  
+}  
+}  
+class TestSuper4{  
+public static void main(String args[]){  
+Dog d=new Dog();  
+}}
