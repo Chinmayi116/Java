7dee05b33063074c421b83b062f8378e696c9378
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-06-03 09:23:44 +0530

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,19 @@
+package javaapplication2;
+class Testarray{
+public static void main(String args[]){
+//inta[]=new int[5];//declaration and instantiation
+//a[0]=10;//initialization
+//a[1]=20;
+//a[2]=70;
+//a[3]=40;
+//a[4]=50;
+inta[]={1,2,3,4,5};//Second format for declaration, initialization
+//traversing array
+//for(inti=0;i<a.length;i++)//length is the property of array
+//System.out.println(a[i]);
+for(int i:a)//Second format using foreach loop
+{
+System.out.println(i);
+}
+}
+}
