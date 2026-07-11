7d2939d6656fc9b07bcd96f6e1c48aa3fdcb1e9e
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-07-11 19:06:04 +0530

    Add Student class with constructor and display method

---
+++ Constructor.java
@@ -0,0 +1,18 @@
+class Student {
+    int id;
+    String name;
+
+    Student() {
+        id = 101;
+        name = "Chinmayi";
+    }
+
+    void display() {
+        System.out.println(id + " " + name);
+    }
+
+    public static void main(String args[]) {
+        Student s = new Student();
+        s.display();
+    }
+}
