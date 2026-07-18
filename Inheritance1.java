47575eec78fa2beef7a789e8586b60683f1952a0
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-07-18 09:34:59 +0530

    Add Vehicle and Car classes with demo

---
+++ Inheritance1.java
@@ -0,0 +1,19 @@
+class Vehicle {
+    void start() {
+        System.out.println("Vehicle started");
+    }
+}
+
+class Car extends Vehicle {
+    void drive() {
+        System.out.println("Car is running");
+    }
+}
+
+public class VehicleDemo {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.start();
+        c.drive();
+    }
+}
