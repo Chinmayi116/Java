a0c642ce97cfb04ef6d77e2d8a34933b5a5e1c60
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-07-24 09:46:29 +0530

    Create AreaofSpace.java

---
+++ AreaofSpace.java
@@ -0,0 +1,17 @@
+class Area {
+
+    void area(int side) {
+        System.out.println("Area of Square = " + (side * side));
+    }
+
+    void area(int length, int breadth) {
+        System.out.println("Area of Rectangle = " + (length * breadth));
+    }
+
+    public static void main(String[] args) {
+        Area obj = new Area();
+
+        obj.area(5);
+        obj.area(4, 6);
+    }
+}
