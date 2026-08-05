8a7811386bdac30b5f372f774795a5820a50022f
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-08-06 00:37:16 +0530

    Create ArrayList Example.java

---
+++ ArrayList Example.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class ArrayListDemo {
+    public static void main(String args[]) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+        list.add("PHP");
+
+        System.out.println(list);
+    }
+}
