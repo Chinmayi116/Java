0de23468c2f1e28e3a4d70f90f53b4dc16bec985
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-08-14 17:24:03 +0530

    Create Constructor12.java

---
+++ Constructor12.java
@@ -0,0 +1,9 @@
+class ConstructorExample {
+    ConstructorExample() {
+        System.out.println("Constructor called");
+    }
+
+    public static void main(String[] args) {
+        ConstructorExample obj = new ConstructorExample();
+    }
+}
