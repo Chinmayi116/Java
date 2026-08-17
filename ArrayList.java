6844e1e06ec9b4fe22ba964037e198ad83d7865a
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-08-17 23:37:04 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Chinmayi");
+        names.add("Rahul");
+        names.add("Priya");
+
+        System.out.println(names);
+    }
+}
