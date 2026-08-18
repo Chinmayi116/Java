7d33abc971f29f2c4077456a61ee4528a953b3f0
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-08-18 19:03:44 +0530

    Create LinkedList1.java

---
+++ LinkedList1.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
